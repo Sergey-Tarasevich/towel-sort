2ba2dd9baae5eddff014c995b1ed3c57a0881192
Sergey Tarasevich <70640645+Sergey-Tarasevich@users.noreply.github.com>
2020-10-05 16:49:10 +0300

    feat: add solution to towel-sort

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
 
-    return [];
+    let arrResult = [];
+
+    for (i = 0; i < matrix.length; i++) {
+
+        if (i % 2 != 0) {
+            matrix[i].reverse();
+        }
+
+        arrResult = arrResult.concat(matrix[i]);
+    }
+
+    return arrResult;
 }
-
